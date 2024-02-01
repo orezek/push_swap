1b19fda8b099c8905004fa2c7236e09c1d955fd0
O.R <orezek@seznam.cz>
2024-02-01 19:03:37 +0100

    Update: implement basic stack and push functions

--- main.c
+++ main.c
@@ -6,15 +6,54 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:11:08 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/02/01 13:18:45 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/02/01 19:01:20 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_init_stack(t_stack *stack)
+{
+	stack->top = NULL;
+	stack->bottom = NULL;
+	stack->size = 0;
+}
+
+// create a node and add it to a stack
+void	ft_push(t_stack *stack, int val)
+{
+	t_node *node;
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return ;
+	node->value = val;
+	node->next = stack->top;
+	node->prev = NULL;
+
+	if (stack->top != NULL)
+		stack->top->prev = node;
+	stack->top = node;
+	stack->size++;
+}
+
 int	main(void)
 {
 	char	*str = "push_swap!\n";
 	ft_printf("%s", str);
+
+	t_stack	stack_a;
+	ft_init_stack(&stack_a);
+
+	for (int i; i < 10; i++)
+	{
+		ft_push(&stack_a, i);
+	}
+	t_node	*current;
+	current = stack_a.top;
+	while(current != NULL)
+	{
+		ft_printf(":%d ", current->value);
+		current = current->next;
+	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 19:27:03 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/02/01 13:34:53 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/02/01 18:12:29 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,16 @@
 # define PUSH_SWAP_H
 # include "libft/libft.h"
 # include <fcntl.h>
+
+typedef struct s_node {
+	int				value;
+	struct s_node	*next;
+	struct s_node	*prev;
+}		t_node;
+
+typedef struct s_stack {
+	t_node	*top;
+	t_node	*bottom;
+	int		size;
+}		t_stack;
 #endif
