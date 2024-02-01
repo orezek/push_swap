1e12b8218f8ae605e91075affc75ee827f8ffa17
O.R <orezek@seznam.cz>
2024-02-01 13:39:32 +0100

    Fix: fix the initial project files

--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,12 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 19:27:03 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/02/01 13:12:20 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/02/01 13:34:53 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 # include "libft/libft.h"
 # include <fcntl.h>
 #endif
