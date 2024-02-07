288f3fe98312e4dcf96284e535ada0b215f9c522
O.R <orezek@seznam.cz>
2024-02-07 15:42:27 +0100

    Implement: write function to cover cases when src > 1 and dst != 0

--- src/push_operations.c
+++ src/push_operations.c
@@ -6,45 +6,62 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 18:29:51 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/02/06 21:50:40 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/02/07 15:40:19 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-
-void	ft_pop_push(t_stack *from_stack, t_stack *to_stack)
+void ft_normal(t_stack *src_stack, t_stack *dst_stack)
 {
-	if (from_stack->size != 0)
-	{
-		t_node	*temp_a;
-		t_node	*temp_b;
+	t_node	*src_temp;
+	t_node	*dst_temp;
 
-		temp_a = NULL;
-		temp_b = NULL;
-		if (from_stack->top->next != NULL)
-			temp_b = from_stack->top->next;
-		if (to_stack->size != 0)
+	src_temp = NULL;
+	// src - ok
+	src_temp = src_stack->top->next;
+	src_temp->prev = NULL;
+	// dst
+	dst_temp = dst_stack->top;
+	// old top.prev = new dst top
+	dst_temp->prev = src_stack->top;
+	// overwrite dst top to new top
+	dst_stack->top = src_stack->top;
+	// link new top to the old top by next
+	dst_stack->top->next = dst_temp;
+	// mark null to the new top prev
+	dst_stack->top->prev = NULL;
+	// remove one from the counter
+	dst_stack->size++;
+	// src - ok
+	src_stack->top = src_temp;
+	src_stack->size--;
+}
+
+
+void	ft_pop_push(t_stack *src_stack, t_stack *dst_stack)
+{
+	if (src_stack->size != 0)
+	{
+		if (src_stack->size == 1 && dst_stack->size == 0)
 		{
-			temp_a = to_stack->top;
-			temp_a->prev = from_stack->top;
+			;
 		}
-		to_stack->top = from_stack->top;
-		to_stack->top->next = temp_a;
-		to_stack->size++;
-		if (to_stack->size == 1 && temp_a != NULL)
-			to_stack->bottom = temp_a;
-		else
-			to_stack->bottom = from_stack->top;
-		if (temp_b != NULL)
-			temp_b->prev = NULL;
-		from_stack->top = temp_b;
-		from_stack->size--;
-		if (from_stack->size == 1)
-			from_stack->bottom = temp_b;
-		else if (from_stack->size == 0)
-			from_stack->bottom = NULL;
+		else if (src_stack->size > 1 && dst_stack->size == 0)
+		{
+			;
+		}
+		else if (src_stack->size == 1 && dst_stack->size != 0)
+		{
+			;
+		}
+		else // src_stack->size > 1 && dst_stack->size != 0
+		{
+			ft_normal(src_stack, dst_stack);
+		}
 	}
+
+	return ;
 }
 
 void	ft_pb(t_stack *from_stack_a, t_stack *to_stack_b)
